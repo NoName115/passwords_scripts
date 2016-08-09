e3ec707a98710718728216a7d7a2ac0b494bca9e
NoName115 <robert.kolcun@gmail.com>
2016-08-09 16:24:29 +0200

    categorize tests in 'tests' folder

--- tests/unit/simpleTest.py
+++ tests/unit/simpleTest.py
@@ -7,13 +7,13 @@ import scripts.loadData as loadData
 class SimpleTestEntropy(unittest.TestCase):
 
 	def testCheckStartEntropy(self):
-		self.passwordData = loadData.LoadFromFile("tests/simpleInput").loadData()
+		self.passwordData = loadData.LoadFromFile("tests/unit/simpleInput").loadData()
 
 		for password, x in zip(self.passwordData, [10, 25, 8, 8, 8, 5, 1, 9]):
 			self.assertEqual(password.entropy, x)
 
 	def testCheckChangedEntropy(self):
-		self.passwordData = loadData.LoadFromFile("tests/simpleInput").loadData()
+		self.passwordData = loadData.LoadFromFile("tests/unit/simpleInput").loadData()
 
 		self.assertIsNone(rules.CapitalizeAllLetters().transform(self.passwordData))
 
