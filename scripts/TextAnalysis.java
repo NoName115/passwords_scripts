01a5b73e25113df1c17ecd7c5e3d6371bb3a0afb
NoName115 <robert.kolcun@gmail.com>
2017-08-04 15:23:36 +0200

    complexFinder added

--- scripts/TextAnalysis.java
+++ scripts/TextAnalysis.java
@@ -7,73 +7,95 @@ import org.owasp.passfault.dictionary.*;
 import org.owasp.passfault.keyboard.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class TextAnalysis
 {
 	public static void main(String [] args)
 	{
+		String password = args[0];
 
-		String password = "1qaz6yhn 11.05.1997";
 		PatternFinder finder = null;
+		ArrayList<PatternFinder> findersCollection = new ArrayList<PatternFinder>();
 
 		// Dictionary finder
-		/*
 		String current = "";
 		FileDictionary dictionary = null;
 
-		try{
-			current = new java.io.File(".").getCanonicalPath();
-		} catch (IOException e)
-		{
-			System.out.println("ERROR: " + e);
-		}
-
-		String filename = "tiny-lower.words";
-		String path = "/core/src/main/java/org/owasp/passfault/";
-
-		path = "/core/src/test/resources/dictionaries/";
+		String mainPath = "/core/src/main/java/org/owasp/passfault";
+		String subPath = "/resources/dictionaries";
+		String filename = "/english.words";
 
 		try
 		{
+			current = new java.io.File(".").getCanonicalPath();
+
 			dictionary = FileDictionary.newInstance(
-				current + path + filename,
+				current + mainPath + subPath + filename,
 				"testovaci_subor"
 				);
 		} catch (IOException e)
 		{
 			System.out.println("ERROR: " + e);
 		}
+		
 
-		finder = new DictionaryPatternsFinder(
-			dictionary,
-			new l337SubstitutionStrategy(),
-			new FilteringPatternCollectionFactory()
-			);
-		*/
-
+		
 		// Date finder
-		finder = new DateFinder(
+		findersCollection.add(new DateFinder(
 			new FilteringPatternCollectionFactory()
-		);
-
-		// Random_class finder
-		/*
-		finder = new RandomClassesFinder(
-			2,
-			new FilteringPatternCollectionFactory()
-			);
-		*/
+		));
 
 		// Key_sequence finder
-		/*
-		finder = new KeySequenceFinder(
-			new EnglishKeyBoard(),
+		findersCollection.add(
+			new KeySequenceFinder(
+				new EnglishKeyBoard(),
+				new FilteringPatternCollectionFactory()
+			)
+		);
+
+		// Reverse pattern finder
+		findersCollection.add(new ReversePatternDecoratorFinder(
+			new DictionaryPatternsFinder(
+				dictionary,
+				new ExactWordStrategy(),
+				new FilteringPatternCollectionFactory()
+			),
+			new FilteringPatternCollectionFactory()
+		));
+
+
+		// Search for patterns
+		CompositeFinder complexFinder = new SequentialFinder(
+			findersCollection,
 			new FilteringPatternCollectionFactory()
 		);
+		
+
+		/*
+		// Not working
+		// TODO
+		// Configurate propsBuilder
+		FinderByPropsBuilder propsBuilder = new FinderByPropsBuilder().setClassPathLoader(
+			"org/owasp/passfault/wordlists/resources"
+		); //loadDefaultWordLists();
+
+		CompositeFinder complexFinder = null;
+
+		try
+		{
+			complexFinder = new SequentialFinder(
+				propsBuilder.build(),
+				new FilteringPatternCollectionFactory()
+			);
+		} catch (IOException e)
+		{
+			System.out.println("ERROR: " + e);
+		}
 		*/
 
-		PatternCollection patterns = finder.search(password);
+		PatternCollection patterns = complexFinder.search(password);
 		AnalysisResult analysisResult = new PatternsAnalyzerImpl().analyze(
 			patterns
 		);
